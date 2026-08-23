c55a06edcd8d50f82d3abd9e240021a748192a1d
DanielFelix-eng <felixjardini502@gmail.com>
2026-08-23 18:28:13 +0300

    final changes

--- api/index.js
+++ api/index.js
@@ -14,10 +14,17 @@ app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
 }))
+app.use(express.static(path.join(__dirname, "../client/dist"))
+)
 
 app.use(express.json())
 app.use(cookieParser())
 
+app.get('*', (req, res) => {
+
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+}
+)
 app.use('/api', authRoutes)
 app.use('/api', checkRoute)
 app.use('/api', keyRoute)
