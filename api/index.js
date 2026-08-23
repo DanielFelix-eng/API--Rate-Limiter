d427c18563a30872a559f433f8443c5837002a8b
DanielFelix-eng <felixjardini502@gmail.com>
2026-08-23 18:58:21 +0300

    url filepath

--- api/index.js
+++ api/index.js
@@ -7,6 +7,11 @@ import authRoutes from './routes/authRoute.js'
 import checkRoute from './routes/checkRoutes.js'
 import keyRoute from './routes/keyRoutes.js'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
